9c8e08e96d2edf9369e9df34cb554ae8ce69ca44
Santos van_der_Wansem <srrvdwansem@gmail.com>
2022-09-30 13:52:45 +0200

    S: opdracht 20

---
+++ santos/opdracht-20-admin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $username = $_GET["username"];
+    $password = $_GET["password"];
+    if ($username == "admin" && $password == "pietje") {
+        echo "Welkom, $username!";
+    } else {
+        echo "Flikker op";
+    };
+    ?>
+</body>
+
+</html>
---
+++ santos/opdracht-20.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opdracht 20</title>
+</head>
+
+<body>
+    <form name="inlog" method="get" action="./opdracht-20-admin.php">
+        <label for="username">Enter your username:</label>
+        <input name="username" id="username" placeholder="username">
+        <br />
+        <label for="password">Enter your password:</label>
+        <input name="password" id="password" placeholder="password">
+        <br />
+        <input type="submit" value="Log in">
+    </form>
+    <?php
+
+    ?>
+</body>
+
+</html>
