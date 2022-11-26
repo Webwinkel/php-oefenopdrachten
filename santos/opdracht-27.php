352d53be2255fe0a4d679f46f61244b220cacda7
Santos van_der_Wansem <srrvdwansem@gmail.com>
2022-11-26 12:35:01 +0100

    opdracht 27

--- santos/opdracht-27.php
+++ santos/opdracht-27.php
@@ -1,16 +1,38 @@
 <!DOCTYPE html>
 <html lang="nl">
-<meta charset="UTF-8">
-<?php
-if (!empty($_GET)) {
-    $naamnieuw = $_GET["naam"];
-    if (file_exists("naam.txt")) {
-        $fp = fopen("naam.txt", "r");
-        $naam = fgets($fp);
-        $fclose($fp);
-        echo "Jouw naam is $naamnieuw";
+
+<head>
+    <meta charset="UTF-8">
+    <?php
+    if (!empty($_GET)) {
+        $naamnieuw = $_GET["naam"];
+        if (file_exists("naam.txt")) {
+            $fp = fopen("naam.txt", "r");
+            $naam = fgets($fp);
+            fclose($fp);
+            echo "Jouw naam is $naamnieuw";
+            if ($naamnieuw != $naam) {
+                echo "<br>Maar vorige keer was je naam nog $naam";
+            }
+            fputs($fp, $naamnieuw);
+            fclose($fp);
+        } else {
+            $fp = fopen("naam.txt", "w");
+            $naamnieuw = $_GET["naam"];
+            fputs($fp, $naamnieuw);
+            fclose($fp);
+        };
     }
-}
-?>
+    ?>
+</head>
+
+<body>
+    <form name="naamform" method="get" action="<?php echo $_SERVER["PHP_SELF"] ?>">
+        <label for="naam">Vertel me je naam</label>
+        <input type="text" name="naam" value="">
+        <br><br>
+        <input type="submit" value="submit" name="submit">
+    </form>
+</body>
 
 </html>
